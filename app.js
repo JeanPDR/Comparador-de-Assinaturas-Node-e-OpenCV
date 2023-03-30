fcf536ec1c06a755444b9c7b7622fe906dcb1c3b
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-03-30 19:15:59 -0300

    UPDATE - Adicionando função para mudar nomes para realizar a comparação com OpenCV

--- app.js
+++ app.js
@@ -13,11 +13,24 @@ const upload = require('./public/js/multerConfig');
 //   res.redirect('/resultado');
 // });
 
+// app.post('/uploads', upload.fields([{ name: 'imagem1', maxCount: 1 }, { name: 'imagem2', maxCount: 1 }]), (req, res) => {
+//   console.log(req.files);
+//   res.redirect('/resultado');
+// });
+
 app.post('/uploads', upload.fields([{ name: 'imagem1', maxCount: 1 }, { name: 'imagem2', maxCount: 1 }]), (req, res) => {
-  console.log(req.files);
+  const img1TempPath = req.files['imagem1'][0].path;
+  const img2TempPath = req.files['imagem2'][0].path;
+
+  const img1NewPath = './uploads/img1.jpg'; // novo nome da imagem 1
+  const img2NewPath = './uploads/img2.jpg'; // novo nome da imagem 2
+
+  fs.renameSync(img1TempPath, img1NewPath);
+  fs.renameSync(img2TempPath, img2NewPath);
+
+  console.log('Imagens renomeadas com sucesso');
   res.redirect('/resultado');
 });
-
 
 
 
